edb1e801868aa140655dbe28951991ade331c2d2
Adam Saponara <as@php.net>
2016-12-29 20:06:10 -0500

    init

---
+++ scrape.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget -r -A gif 'http://cultofthepartyparrot.com/parrots/'
+rm -rf parrots
+mv cultofthepartyparrot.com/parrots/ .
+rm -rf cultofthepartyparrot.com
+for p in $(find parrots/ -name '*gif'); do
+    convert -coalesce "$p" "$p.%02d.png"
+    n=$(ls -1 $p.* | wc -l)
+    echo "'$p': $n,"
+done
